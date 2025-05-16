8c9e9170801057dfb9bcde5f64c4d9ae9b99b2de
bencsii <benicke001@gmail.com>
2025-05-16 11:45:32 +0200

    Program.cs teszteles. Kerdeske szama kiiratva ( osszesen ) valamint kis demo ( csak teszt nem a jatek )

--- legyen_on_is/Program.cs
+++ legyen_on_is/Program.cs
@@ -1,10 +1,29 @@
 ﻿namespace legyen_on_is
 {
-	internal class Program
-	{
-		static void Main(string[] args)
-		{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
 
-		}
-	}
+            Kerdesek kerdesLista = new Kerdesek();
+            kerdesLista.Beolvas("kerdes.txt");
+            Console.WriteLine($"Kerdesek szama : {kerdesLista.KerdesLista.Count}");
+            Kerdes kerdes = kerdesLista.KerdesLista[3];
+
+            Console.WriteLine($"Kategoria: {kerdes.Kategoria}");
+            Console.WriteLine($"Kerdes: {kerdes.KerdesSz}");
+
+            char betu = 'A';
+            foreach (string valasz in kerdes.ValaszokLista)
+            {
+                Console.WriteLine($"{betu} {valasz}");
+                betu++;
+            }
+
+            Console.WriteLine($"Helyes valasz: {kerdes.HelyesValasz}");
+
+        }
+    }
 }
+
